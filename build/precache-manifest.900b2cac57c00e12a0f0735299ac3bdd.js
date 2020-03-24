9da918d7b91797f30d88d06739cabcb43be93eeb
travis-john <travis.wl.john@gmail.com>
2020-03-23 23:51:14 -0400

    pushing build folder

---
+++ build/precache-manifest.900b2cac57c00e12a0f0735299ac3bdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0418fd2ac023bde2277a4741a366c4b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12b0afedcf82b0048632",
+    "url": "/static/js/2.a946ff75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a946ff75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132a95c60c2a4bbb1b76",
+    "url": "/static/js/main.7c6f5c60.chunk.js"
+  },
+  {
+    "revision": "f277f6d70e726ee1c57f",
+    "url": "/static/js/runtime-main.cb69733b.js"
+  }
+]);
