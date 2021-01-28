6c2f6f91340982dee2223544c2eed7e4c65f3284
travis-john <travis.wl.john@gmail.com>
2021-01-28 13:22:57 -0500

    scrolltop component styling

--- src/components/ScrollTop.js
+++ src/components/ScrollTop.js
@@ -1,7 +1,37 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faArrowUp} from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-scroll";
 
-class ScrollTop extends Component {
-  
+const ScrollTop = (props) => {
+
+  useEffect(()=>{
+
+    window.addEventListener('scroll', handleScroll);
+
+  });
+
+  let [scrolling, updatehandlescroll] = useState(false);
+
+  const handleScroll = (props) => {
+
+  window.onscroll = function() {
+      if(window.pageYOffset === 0) {
+        updatehandlescroll(scrolling=false)
+      } else {
+        updatehandlescroll(scrolling=true);
+      }
+    };
+  }
+
+  return (
+    <>
+      <div className={ (scrolling===true )? (`scrolltop--container` ): (`scrolltop--container d-none`)}>
+        <FontAwesomeIcon className='fas scroll-top--icon' icon={faArrowUp} />
+      </div>
+    </>
+  )
+
 }
+
+export default ScrollTop;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import MainNav from '../components/MainNav';
 import Masthead from '../components/Masthead';
 import Showcase from '../components/Showcase';
 import Contact from '../components/Contact';
+import ScrollTop from '../components/ScrollTop';
 import Footer from '../components/Footer';
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
     <Masthead />
     <Showcase />
     <Contact />
+    <ScrollTop />
     <Footer />
     </>
   )
