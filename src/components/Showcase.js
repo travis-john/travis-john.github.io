f96fa396cff4968e7f9e66885d0328d9651ea04f
travis-john <travis.wl.john@gmail.com>
2021-01-29 09:22:53 -0500

    changing showcase projects and adding shadow to Showcase h2

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons';
 import {faGithub, faCodepen} from '@fortawesome/free-brands-svg-icons';
+import { faDownload  } from '@fortawesome/free-solid-svg-icons';
 
 class Showcase extends Component {
 
@@ -25,21 +26,53 @@ class Showcase extends Component {
     return (
       <div className='bg-showcase p-5' id='showcase'>
       <Container>
-        <h2 className='text-center'>Noteable Projects</h2>
+        <h2 className='text-center text-white text-shadow'>Noteable Projects</h2>
         <Row className='p-2'>
           <CardDeck>
             <Card className='mb-3 rounded-2 border-left-primary'>
               <CardBody>
-              <CardTitle><h3>movieBrowser</h3></CardTitle>
-              <CardSubtitle>Feb 2020</CardSubtitle>
-              <CardText>A movie browsing application build using MaterializeCSS and HandlebarsJS on the front end, MYSQL database, and Express</CardText>
-              <CardLink href='https://whispering-dawn-54949.herokuapp.com/' target='_blank'>View application<FontAwesomeIcon className='ml-2' icon={faExternalLinkAlt} /><span className='sr-only'>opens in new tab</span></CardLink>
-              <CardLink href='/development/movieBrowser'>More details</CardLink>
+              <CardTitle><h3>Email Template Library</h3></CardTitle>
+              <CardSubtitle>Oct 2020 - Jan 2021</CardSubtitle>
+              <CardText>A library of uniquely branded email templates designed for for Cisco CX</CardText>
+              <CardLink href='/design/email-template-library'>More details</CardLink>
               </CardBody>
               <CardFooter>
-                <CardLink className='card-footer-link' href='https://github.com/apereira91/project2'> <FontAwesomeIcon className='ml-auto' icon={faGithub} /><span className='sr-only'>opens in new tab></span> </CardLink>
+                <CardLink className='card-footer-link' href='/assets/images/design/email-template-library.sketch'> <FontAwesomeIcon className='ml-auto' icon={faDownload} /><span className='sr-only'>download prototype></span> </CardLink>
               </CardFooter>
             </Card>
+          <Card className='mb-3 rounded-2 border-left-primary'>
+            <CardBody>
+            <CardTitle><h3>Card Component</h3></CardTitle>
+            <CardSubtitle>Apr 2020</CardSubtitle>
+            <CardText>A proposed new card component for Cisco.com</CardText>
+            <CardLink href='/design/card-component'>More details</CardLink>
+            </CardBody>
+            <CardFooter>
+              <CardLink className='card-footer-link' href='/assets/images/design/card-component.sketch'> <FontAwesomeIcon className='ml-auto' icon={faDownload} /><span className='sr-only'>download prototype></span> </CardLink>
+            </CardFooter>
+          </Card>
+          <Card className='mb-3 rounded-2 border-left-primary'>
+            <CardBody>
+            <CardTitle><h3>Cisco.com Pages</h3></CardTitle>
+            <CardSubtitle>Oct 2019 - Jan 2021</CardSubtitle>
+            <CardText>Page templages and experience for DLJ-owned pages on Cisco.com</CardText>
+            <CardLink href='/design/dlj-section'>More details</CardLink>
+            </CardBody>
+          </Card>
+          </CardDeck>
+          <CardDeck>
+          <Card className='mb-3 rounded-2 border-left-primary'>
+            <CardBody>
+            <CardTitle><h3>movieBrowser</h3></CardTitle>
+            <CardSubtitle>Feb 2020</CardSubtitle>
+            <CardText>A movie browsing application build using MaterializeCSS and HandlebarsJS on the front end, MYSQL database, and Express</CardText>
+            <CardLink href='https://whispering-dawn-54949.herokuapp.com/' target='_blank'>View application<FontAwesomeIcon className='ml-2' icon={faExternalLinkAlt} /><span className='sr-only'>opens in new tab</span></CardLink>
+            <CardLink href='/development/movieBrowser'>More details</CardLink>
+            </CardBody>
+            <CardFooter>
+              <CardLink className='card-footer-link' href='https://github.com/apereira91/project2'> <FontAwesomeIcon className='ml-auto' icon={faGithub} /><span className='sr-only'>opens in new tab></span> </CardLink>
+            </CardFooter>
+          </Card>
           <Card className='mb-3 rounded-2 border-left-primary'>
             <CardBody>
             <CardTitle><h3>Crypto Dashboard</h3></CardTitle>
@@ -54,31 +87,6 @@ class Showcase extends Component {
           </Card>
           <Card className='mb-3 rounded-2 border-left-primary'>
             <CardBody>
-            <CardTitle><h3>Star Wars Crawls</h3></CardTitle>
-            <CardSubtitle>Feb 2020</CardSubtitle>
-            <CardText>Using JQuery AJAX calls, CSS Animations, and the free SWAPI to animate the opening crawls from all Star Wars movies</CardText>
-            <CardLink href='https://travis-john.github.io/swapi-movie-crawl-animation' target='_blank'>View application<FontAwesomeIcon className='ml-2' icon={faExternalLinkAlt} /><span className='sr-only'>opens in new tab</span></CardLink>
-            </CardBody>
-            <CardFooter>
-              <CardLink className='card-footer-link' href='https://github.com/travis-john/dolla-dolla-bills'><FontAwesomeIcon className='ml-auto' icon={faGithub} /> <span className='sr-only'>opens in new tab></span> </CardLink>
-            </CardFooter>
-          </Card>
-          </CardDeck>
-          <CardDeck>
-          <Card className='mb-3 rounded-2 border-left-primary'>
-            <CardBody>
-            <CardTitle><h3>USWNT ZDOG</h3></CardTitle>
-            <CardSubtitle>Jun 2019</CardSubtitle>
-            <CardText>Zdog is a new Javascript library that renders pseudo-3D elements onto a canvas. I experimented with it's semantic class structure to combine shapes and paths to recreate a 3D rendering of the United States Women's National Football Team</CardText>
-            <CardLink href='https://codepen.io/travis_john/full/agbMNx' target='_blank'>View application<FontAwesomeIcon className='ml-2' icon={faExternalLinkAlt} /><span className='sr-only'>opens in new tab</span></CardLink>
-            <CardLink href='/development/zdog-uswnt'>More details</CardLink>
-            </CardBody>
-            <CardFooter>
-              <CardLink className='card-footer-link' href='https://codepen.io/travis_john/pen/agbMNx'><FontAwesomeIcon className='ml-auto' icon={faCodepen} /> <span className='sr-only'>opens in new tab></span> </CardLink>
-            </CardFooter>
-          </Card>
-          <Card className='mb-3 rounded-2 border-left-primary'>
-            <CardBody>
             <CardTitle><h3>Gooey Loaders</h3></CardTitle>
             <CardSubtitle>May 2019</CardSubtitle>
             <CardText>Prototype of a loader component that uses SVG animations and filters to create a gooey effect</CardText>
@@ -86,18 +94,6 @@ class Showcase extends Component {
             </CardBody>
             <CardFooter>
               <CardLink className='card-footer-link' href='https://codepen.io/travis_john/full/xMNLam'><FontAwesomeIcon className='ml-auto' icon={faCodepen} /> <span className='sr-only'>opens in new tab></span> </CardLink>
-            </CardFooter>
-          </Card>
-          <Card className='mb-3 rounded-2 border-left-primary'>
-            <CardBody>
-            <CardTitle><h3>Falcon Toggle</h3></CardTitle>
-            <CardSubtitle>May 2019</CardSubtitle>
-            <CardText>A toggle component styled after the jump to hyperspace</CardText>
-            <CardLink href='https://codepen.io/travis_john/pen/dEqqzq'>View application<FontAwesomeIcon className='ml-2' icon={faExternalLinkAlt} /><span className='sr-only'>opens in new tab</span></CardLink>
-            <CardLink href='/development/falcon-toggle'>More details</CardLink>
-            </CardBody>
-            <CardFooter>
-              <CardLink className='card-footer-link' href='https://codepen.io/travis_john/full/dEqqzq'><FontAwesomeIcon className='ml-auto' icon={faGithub} /> <span className='sr-only'>opens in new tab></span> </CardLink>
             </CardFooter>
           </Card>
           </CardDeck>
